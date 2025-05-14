d7f06b23555ffd96e9f958bdca7fc64fba87ed6c
mitchieb <mitchbart@proton.me>
2025-05-14 16:05:07 +1000

    first commit

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import Express, { Request, Response } from 'express';
+// import indexRoute from './routes';
+
+// Initiate express
+const app = Express();
+app.use(Express.json());
+
+// app.use(indexRoute);
+
+
+const port = process.env.PORT || 3000;
+
+
+
+// Setup “hello world” endpoint
+//Use middleware for catch-all
+// app.use((req: Request, res: Response) => {
+//     res.send('Hello World!');
+// });
+
+
+// Start the express server on the relevant port
+app.listen(port, () => {
+  console.log(`server is running on ${port}`);
+});
