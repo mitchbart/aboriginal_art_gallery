a5bf19949975ebcb387410693ebb618ba907d1a8
mitchieb <mitchbart@proton.me>
2025-05-14 19:56:10 +1000

    fixed some bugs

--- src/client.ts
+++ src/client.ts
@@ -1,7 +1,6 @@
 // Create a prisma client instance - used for database interaction
 
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '../generated/prisma';
 
 const prisma = new PrismaClient();
-
 export default prisma;
